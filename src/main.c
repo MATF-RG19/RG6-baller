170742d23262dd4b9ec2dc83725a842b5d5cdc70
gavrilovicm <gavrilovicmiodrag4@gmail.com>
2020-01-16 19:15:51 +0100

    Dokumentovan kod za propadanje polica

--- src/main.c
+++ src/main.c
@@ -510,6 +510,10 @@ void animiraj_slobodan_pad(){
 	}
 }
 
+/**
+ * funkcija koja implementira pad polica kada se aktivira.
+ * @param value - jedinstveni ID ove funkcije
+ */
 void pocni_pad(int value){
 	if (value != 999){
 		return;
@@ -798,6 +802,8 @@ static void on_display(void)
 	//iscrtavanje novcica za bonuse
 	iscrtaj_novcice(move, ball_y_coord, novcici, &brojac_novcica);
 
+	// ovde proveravamo dokle je pozicija dosla i da li treba da 
+	// pustimo podlogu da pada 
 	if ((int)(move-0.6) % 12 == 0 && move > 4){
 		if (!podloga_pada){
 			glutTimerFunc(20, pocni_pad, 999);
